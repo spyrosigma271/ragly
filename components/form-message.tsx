82832d4b3e2c332aea68a856a1d176cab812a8c3
Satyam Namdev <namdev2003satyam@gmail.com>
2024-12-03 22:04:02 +0530

    Fixed UI of form-message

--- components/form-message.tsx
+++ components/form-message.tsx
@@ -6,21 +6,23 @@ export type Message =
 export function FormMessage({ message }: { message: Message }) {
   return (
     <div className="flex flex-col gap-2 w-full max-w-md text-sm text-black dark:text-white">
-      {"success" in message && (
-        <div className="text-green-600 dark:text-green-400 border-l-2 border-green-600 dark:border-green-400 px-4">
-          {message.success}
-        </div>
-      )}
-      {"error" in message && (
-        <div className="text-red-600 dark:text-red-400 border-l-2 border-red-600 dark:border-red-400 px-4">
-          {message.error}
-        </div>
-      )}
-      {"message" in message && (
-        <div className="text-black dark:text-foreground border-l-2 px-4">
-          {message.message}
-        </div>
-      )}
+      <div className="w-full box-border">
+        {"success" in message && (
+          <div className="text-green-600 dark:text-green-400 border-l-2 border-green-600 dark:border-green-400 px-4">
+            {message.success}
+          </div>
+        )}
+        {"error" in message && (
+          <div className="text-red-600 dark:text-red-400 border-l-2 border-red-600 dark:border-red-400 px-4">
+            {message.error}
+          </div>
+        )}
+        {"message" in message && (
+          <div className="text-black dark:text-foreground border-l-2 px-4">
+            {message.message}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
